6465827e4024fad6f5f6f2b891f5760694dd74c3
Garvit Kulshrestha <garvit192900@dei.ac.in>
2023-08-17 15:36:26 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def get_air_quality(city):
+    api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+    response = requests.get(api_url, headers={'X-Api-Key': 'cghnjSy9G+prIJf6XREa0g==cSV3F0Bc1AhdMdxc'})
+    if response.status_code == requests.codes.ok:
+        data = json.loads(response.text)
+        return data
+    else:
+        return Exception("Error: {} {}".format(response.status_code, response.text))
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['city']
+        air_quality = get_air_quality(city)
+        return render_template('results.html', result=air_quality, city=city)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
